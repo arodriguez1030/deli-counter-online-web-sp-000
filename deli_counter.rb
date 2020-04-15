026571ab1fc9b2967e41df36d9790ffaf4a3a6cc
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-15 19:30:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
 if katz_deli.length == 0
  puts "The line is currently empty."
  else
-   puts "The line is currently: 
+   counter = 1
+   puts "The line is currently: { "
 end
 end
