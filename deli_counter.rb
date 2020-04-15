f45c45a23ab19f282260a4260ff23c26164a4915
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-15 19:44:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ if array.length == 0
  else
    other_deli.each do |name|
    counter = 1
-  "The line is currently: {#counter}. {#name}"
+   "The line is currently: {#counter}. {#name}"
    counter +=1
 end
 end
