8ef3ef6ef47f77bd7aca663534f67609c000aa39
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-15 19:39:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ if katz_deli.length == 0
  puts "The line is currently empty."
  else
    counter = 1
-   puts "The line is currently: { "
+   puts "The line is currently: {#counter}. {#name}"
 end
 end
