bf266145cbdca1fbdc740a02a96faf8bd5c65c07
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-15 19:50:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
 if array.length == 0
  puts "The line is currently empty."
  else
-   other_deli.each_with_index do |name|
+   other_deli.each_with_index do {|name, index|
    puts "The line is currently: {#counter}. {#name}"
    counter +=1
 end
